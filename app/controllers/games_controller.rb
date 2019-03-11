45d2d39bf2fa416b1af1b56dcb79cc4d6584a0a6
Felice Forby <feliceforby@gmail.com>
2019-03-11 22:38:16 +0000

    Automatically backed up by Learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -19,7 +19,7 @@ class GamesController < ApplicationController
 
   def update
     @game = Game.find_by(id: params[:id])
-    @game.update({state: params[:state])
+    @game.update({state: params[:state]})
 
     render json: @game, status: 200
   end

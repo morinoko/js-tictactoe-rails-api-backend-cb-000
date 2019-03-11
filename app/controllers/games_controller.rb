c483eb69abde3cb74ea372f05334493b74d86581
Felice Forby <feliceforby@gmail.com>
2019-03-11 22:33:15 +0000

    Automatically backed up by Learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,26 +1,26 @@
 class GamesController < ApplicationController
   def index
     @games = Game.all
-    
+
     render json: @games, status: 200
   end
 
   def show
     @game = Game.find_by(id: params[:id])
-    
+
     render json: @game, status: 200
   end
 
   def create
     @game = Game.create(game_params)
-    
+
     render json: @game, status: 201
   end
 
   def update
     @game = Game.find_by(id: params[:id])
     @game.update(game_params)
-    
+
     render json: @game, status: 200
   end
 
